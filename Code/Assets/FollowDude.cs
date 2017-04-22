247e0482edf9e60eef4280dd22b55c6a548c0516
Balestrieri Julien <julien.balestrieri@gmail.com>
2017-04-22 17:11:51 +0200

    add subdude

---
+++ Code/Assets/FollowDude.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FollowDude : MonoBehaviour
+{
+
+    public GameObject m_planet;
+    public GameObject m_dude;
+    public float m_radius;
+    public float m_speed;
+    public float m_angle;
+
+    void Update()
+    {
+        transform.position = m_planet.transform.position + m_radius * new Vector3(Mathf.Cos(m_angle), Mathf.Sin(m_angle), 0f);
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_angle += Time.deltaTime * m_speed * m_dude.GetComponent<MovementBehaviour>().m_leftSpeed;
+            if (m_angle > Mathf.PI * 2)
+            {
+                m_angle -= Mathf.PI * 2;
+            }
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            m_angle -= Time.deltaTime * m_speed * m_dude.GetComponent<MovementBehaviour>().m_rightSpeed;
+            if (m_angle < 0)
+            {
+                m_angle += Mathf.PI * 2;
+            }
+        }
+    }
+}
--- Code/Assets/Scripts/MovementBehaviour.cs
+++ Code/Assets/Scripts/MovementBehaviour.cs
@@ -3,57 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class MovementBehaviour : MonoBehaviour {
+public class MovementBehaviour : MonoBehaviour
+{
 
-	private float m_leftSpeed = 1;
-	private float m_rightSpeed = 1;
+    public float m_leftSpeed = 1;
+    public float m_rightSpeed = 1;
 
     public GameObject m_planet;
+    public GameObject m_parentDude;
     public float m_radius;
     public float m_speed;
     public float m_angle;
 
-	void Update ()
+    void Update()
     {
 
         transform.position = m_planet.transform.position + m_radius * new Vector3(Mathf.Cos(m_angle), Mathf.Sin(m_angle), 0f);
+
+        float leftSpeed = m_leftSpeed;
+        float rightSpeed = m_rightSpeed;
+
+        if (m_parentDude != null && m_parentDude != this)
+        {
+            leftSpeed *= m_parentDude.GetComponent<MovementBehaviour>().m_leftSpeed;
+            rightSpeed *= m_parentDude.GetComponent<MovementBehaviour>().m_rightSpeed;
+        }
+
+        if (m_parentDude == null)
+            return;
+
         if (Input.GetKey(KeyCode.A))
         {
-            m_angle += Time.deltaTime * m_speed * m_leftSpeed;
-			if (m_angle > Mathf.PI * 2)
-			{
-				m_angle -= Mathf.PI * 2;
-			}
+            m_angle += Time.deltaTime * m_speed * leftSpeed;
+            if (m_angle > Mathf.PI * 2)
+            {
+                m_angle -= Mathf.PI * 2;
+            }
         }
+
         if (Input.GetKey(KeyCode.E))
         {
-            m_angle -= Time.deltaTime * m_speed * m_rightSpeed;
-			if (m_angle < 0)
-			{
-				m_angle += Mathf.PI * 2;
-			}
+            m_angle -= Time.deltaTime * m_speed * rightSpeed;
+            if (m_angle < 0)
+            {
+                m_angle += Mathf.PI * 2;
+            }
         }
-
-		//m_angle = m_angle % (2 * Mathf.PI);
     }
 
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		Debug.Log("angle crate : " + col.gameObject.GetComponent<PositionBehaviour>().m_angle);
-		Debug.Log("angle dude : " + m_angle);
-		if (col.gameObject.GetComponent<PositionBehaviour>().m_angle > m_angle)
-		{
-			m_leftSpeed = 0;
-		}
-		else
-		{
-			m_rightSpeed = 0;
-		}
-	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<PositionBehaviour>().m_angle > m_angle)
+        {
+            m_leftSpeed = 0;
+        }
+        else
+        {
+            m_rightSpeed = 0;
+        }
+    }
 
-	void OnCollisionExit2D(Collision2D col)
-	{
-		m_leftSpeed = 1;
-		m_rightSpeed = 1;
-	}
+    void OnCollisionExit2D(Collision2D col)
+    {
+        m_leftSpeed = 1;
+        m_rightSpeed = 1;
+    }
 }

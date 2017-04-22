6191ea6e7a21cf696d05abc381ce5796b2aca888
Marion Maignan <maigmarion@gmail.com>
2017-04-22 16:41:16 +0200

    Huge update of death

--- Code/Assets/Scripts/MovementBehaviour.cs
+++ Code/Assets/Scripts/MovementBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class MovementBehaviour : MonoBehaviour {
 
+	private float m_leftSpeed = 1;
+	private float m_rightSpeed = 1;
+
     public GameObject m_planet;
     public float m_radius;
     public float m_speed;
@@ -12,14 +15,45 @@ public class MovementBehaviour : MonoBehaviour {
 
 	void Update ()
     {
+
         transform.position = m_planet.transform.position + m_radius * new Vector3(Mathf.Cos(m_angle), Mathf.Sin(m_angle), 0f);
         if (Input.GetKey(KeyCode.A))
         {
-            m_angle += Time.deltaTime * m_speed;
+            m_angle += Time.deltaTime * m_speed * m_leftSpeed;
+			if (m_angle > Mathf.PI * 2)
+			{
+				m_angle -= Mathf.PI * 2;
+			}
         }
         if (Input.GetKey(KeyCode.E))
         {
-            m_angle -= Time.deltaTime * m_speed;
+            m_angle -= Time.deltaTime * m_speed * m_rightSpeed;
+			if (m_angle < 0)
+			{
+				m_angle += Mathf.PI * 2;
+			}
         }
+
+		//m_angle = m_angle % (2 * Mathf.PI);
     }
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Debug.Log("angle crate : " + col.gameObject.GetComponent<PositionBehaviour>().m_angle);
+		Debug.Log("angle dude : " + m_angle);
+		if (col.gameObject.GetComponent<PositionBehaviour>().m_angle > m_angle)
+		{
+			m_leftSpeed = 0;
+		}
+		else
+		{
+			m_rightSpeed = 0;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col)
+	{
+		m_leftSpeed = 1;
+		m_rightSpeed = 1;
+	}
 }
--- Code/Assets/Scripts/PositionBehaviour.cs
+++ Code/Assets/Scripts/PositionBehaviour.cs
@@ -7,12 +7,21 @@ public class PositionBehaviour : MonoBehaviour {
 
     public GameObject m_planet;
     public float m_radius;
-    public float m_speed;
     public float m_angle;
 
-	void Start()
+	void Update()
     {
         transform.up = transform.position - m_planet.transform.position;
         transform.position = m_planet.transform.position + m_radius * new Vector3(Mathf.Cos(m_angle), Mathf.Sin(m_angle), 0f);
+
+		if (m_angle > Mathf.PI * 2)
+		{
+			m_angle -= Mathf.PI * 2;
+		}
+
+		if (m_angle < 0)
+		{
+			m_angle += Mathf.PI * 2;
+		}
     }
 }

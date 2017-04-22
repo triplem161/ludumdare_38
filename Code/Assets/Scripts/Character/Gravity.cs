8d0e7261942614ac4e16b785c8c28350067ff39a
Triplem <maigmarion@gmail.com>
2017-04-22 11:53:20 +0200

    First commit

---
+++ Code/Assets/Scripts/Character/Gravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+	void FixedUpdate()
+	{
+		GetGravityDirection();
+	}
+
+	private void GetGravityDirection()
+	{
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+		if (hit.collider != null)
+		{
+			this.transform.up = hit.normal;
+		}
+	}
+}

271c64f41f13637788e8ae3b081053103fc0edd6
Triplem <maigmarion@gmail.com>
2017-04-23 13:44:51 +0200

    Added function to check if child and parent are link

--- Code/Assets/Scripts/MovementBehaviour.cs
+++ Code/Assets/Scripts/MovementBehaviour.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class MovementBehaviour : MonoBehaviour
 {
-
     public float m_leftSpeed = 1;
     public float m_rightSpeed = 1;
-
     public GameObject m_planet;
     public GameObject m_parentDude;
+	public GameObject m_childDude;
     public float m_radius;
     public float m_speed;
     public float m_angle;
@@ -51,7 +50,42 @@ public class MovementBehaviour : MonoBehaviour
 		{
 			m_angle += Mathf.PI * 2;
 		}
+
     }
+
+	public bool isLinkToChild()
+	{
+		float childAngle = m_childDude.GetComponent<MovementBehaviour>().m_angle;
+
+		if (childAngle > m_angle - 0.01 && childAngle < m_angle + 0.01 && m_childDude != null)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+		// Do NOT WORK and i dunno why, the this.m_angle is different from its real value
+//		return childAngle > m_angle - 0.01 && childAngle < m_angle + 0.01;
+	}
+
+	public bool isLinkToParent()
+	{
+		if (m_parentDude == null || m_parentDude == this.gameObject)
+			return false;
+
+		float parentAngle = m_parentDude.GetComponent<MovementBehaviour>().m_angle;
+
+		if (parentAngle > m_angle - 0.01 && parentAngle < m_angle + 0.01)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
     void OnCollisionEnter2D(Collision2D col)
     {
